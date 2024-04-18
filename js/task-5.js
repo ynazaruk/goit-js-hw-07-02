706577eb0d07e5be58aa3d7e5bed994ddcdaa35d
ynazaruk <nazaruk.ya@gmail.com>
2024-04-18 23:20:11 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorValue = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", onClickBtn);
+
+function onClickBtn(event) {
+  colorValue.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorValue.textContent;
+}
