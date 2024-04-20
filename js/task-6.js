501602857f6d8376ae853866a16e9471dd4c809d
ynazaruk <nazaruk.ya@gmail.com>
2024-04-20 20:01:59 +0300

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const numberInput = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const divBox = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", (event) => {
+  if (numberInput.value >= 1 && numberInput.value <= 100) {
+    createBoxes(numberInput.value);
+    numberInput.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+function createBoxes(amount) {
+  divBox.innerHTML = "";
+  for (let i = 0; i < amount; i += 1) {
+    let boxSize = 30 + i * 10;
+    divBox.insertAdjacentHTML(
+      "beforeend",
+      `<div style = "width: ${boxSize}px; height: ${boxSize}px; background: ${getRandomHexColor()}; margin-bottom: 5px"></div>`
+    );
+  }
+}
+
+destroyBtn.addEventListener("click", (event) => {
+  divBox.innerHTML = "";
+});
